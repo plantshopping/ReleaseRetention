22a99ad5ebd7c9c711979a4b84cb3f571b2966fe
Amy Lin <xiaohui@outlook.co.nz>
2021-10-06 18:21:21 +1300

    Add initial tests

--- ReleasesRetention.Test/ReleasesRetentionTest.cs
+++ ReleasesRetention.Test/ReleasesRetentionTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ReleasesRetention.Test
@@ -15,13 +17,44 @@ namespace ReleasesRetention.Test
         public void Calculate_OneReleaseToKeep_ReturnsLatestRelease()
         {
             // Arrange
+            var projects = new List<Project> { new Project { Id = "538fc083-130b-48cb-978b-5960e3b29093", Name = "Pets" } };
 
+            var releases = new List<Release> { new Release { Id = "d943de9c-8a83-49f7-b301-a06e6ab176a2", ProjectId = "538fc083-130b-48cb-978b-5960e3b29093", Version = "0.0.1", Created = new DateTime(2000, 1, 1) } };
+
+            var deployment = new List<Deployment> { new Deployment { Id = "0d02e3e6-3b00-4f54-9d03-d37f1e613a0f", ReleaseId = "d943de9c-8a83-49f7-b301-a06e6ab176a2", EnvironmentId = "b29d67d1-dd1d-4c71-b144-6004c4cf2b03", DeployedAt = new DateTime(2000, 1, 1) } };
+
+            var environment = new List<Environment> { new Environment { Id = "b29d67d1-dd1d-4c71-b144-6004c4cf2b03", Name = "Staging" } };
 
             // Act
-            var result = releaseRetention.CalculateRetention(0);
+            var result = releaseRetention.CalculateRetention(projects, releases, deployment, environment, 0);
 
             // Assert
-            Assert.Empty(result);
+            var expectedResult = new ReleaseRetentionResult { ReleaseId = "d943de9c-8a83-49f7-b301-a06e6ab176a2", EnvironmentId = "b29d67d1-dd1d-4c71-b144-6004c4cf2b03" };
+
+            Assert.Equal(expectedResult.ReleaseId, result[0].ReleaseId);
+            Assert.Equal(expectedResult.EnvironmentId, result[1].ReleaseId);
+        }
+
+        [Fact]
+        public void Calculate_TwoReleasesToKeep_ReturnsLatestRelease()
+        {
+            // Arrange
+            var projects = new List<Project> { new Project { Id = "538fc083-130b-48cb-978b-5960e3b29093", Name = "Pets" } };
+
+            var releases = new List<Release> { new Release { Id = "d943de9c-8a83-49f7-b301-a06e6ab176a2", ProjectId = "538fc083-130b-48cb-978b-5960e3b29093", Version = "0.0.1", Created = new DateTime(2000, 1, 1) }, new Release { Id = "62976be4-253a-4dd1-8814-89aea49a4364", ProjectId = "538fc083-130b-48cb-978b-5960e3b29093", Version = "0.0.2", Created = new DateTime(2000, 1, 1) } };
+
+            var deployment = new List<Deployment> { new Deployment { Id = "0d02e3e6-3b00-4f54-9d03-d37f1e613a0f", ReleaseId = "62976be4-253a-4dd1-8814-89aea49a4364", EnvironmentId = "b29d67d1-dd1d-4c71-b144-6004c4cf2b03", DeployedAt = new DateTime(2000, 1, 1) } };
+
+            var environment = new List<Environment> { new Environment { Id = "b29d67d1-dd1d-4c71-b144-6004c4cf2b03", Name = "Staging" } };
+
+            // Act
+            var result = releaseRetention.CalculateRetention(projects, releases, deployment, environment, 0);
+
+            // Assert
+            var expectedResult = new ReleaseRetentionResult { ReleaseId = "62976be4-253a-4dd1-8814-89aea49a4364", EnvironmentId = "b29d67d1-dd1d-4c71-b144-6004c4cf2b03" };
+
+            Assert.Equal(expectedResult.ReleaseId, result[0].ReleaseId);
+            Assert.Equal(expectedResult.EnvironmentId, result[1].ReleaseId);
         }
     }
 }
--- ReleasesRetention/ReleaseRetention.cs
+++ ReleasesRetention/ReleaseRetention.cs
@@ -11,6 +11,8 @@ namespace ReleasesRetention
         public List<ReleaseRetentionResult> CalculateRetention(List<Project> projects, List<Release> releases, List<Deployment> deployments, List<Environment> environments, int numberOfReleasesToKeep)
         {
             // TODO: Assume if number of releases to keep is 0, we return all
+
+            // TODO: Look at DeployedAt time to determine the latest release
             return new List<ReleaseRetentionResult>();
         }
     }
